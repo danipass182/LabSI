0f041333cf7ee408df01a53fc3559881bd74e58c
ross <ross69200@gmail.com>
2022-05-27 15:51:33 +0200

    test

---
+++ src/main/java/it/concessionaria/rendemezzi/RendeMezziApplication.java
@@ -0,0 +1,13 @@
+package it.concessionaria.rendemezzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendeMezziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RendeMezziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/concessionaria/rendemezzi/RendeMezziApplicationTests.java
@@ -0,0 +1,13 @@
+package it.concessionaria.rendemezzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RendeMezziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
